366dc21550faac618d0c6e430e0674394036076d
Anduin2017 <anduin@aiursoft.com>
2019-10-16 15:19:42 +0800

    Remove more aiursoft related items.

--- src/Models/ColossusUser.cs
+++ src/Models/ColossusUser.cs
@@ -1,4 +1,6 @@
-﻿namespace BopodaMVP.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BopodaMVP.Models
 {
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class MVPUser
@@ -6,5 +8,24 @@
         public string Id { get; set; }
         public string SiteName { get; set; }
         public string Name { get; internal set; }
+    }
+
+    public class Tenant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class TenantUserRelationship
+    {
+        public int Id { get; set; }
+
+        public int TenantId { get; set; }
+        [ForeignKey(nameof(TenantId))]
+        public Tenant Tenant { get; set; }
+
+        public string UserId { get; set; }
+        [ForeignKey(nameof(UserId))]
+        public MVPUser User { get; set; }
     }
 }
