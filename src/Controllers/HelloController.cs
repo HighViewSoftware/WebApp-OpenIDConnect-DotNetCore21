245f243c7a6aa8f5076bf9fd99301f9c318e1769
Anduin2017 <anduin@aiursoft.com>
2019-10-17 15:13:01 +0800

    Create a simple API.

---
+++ src/Controllers/HelloController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BopodaMVP.Controllers
+{
+    public class HelloController : Controller
+    {
+        public IActionResult Index()
+        {
+            var id = User.Claims.SingleOrDefault(t => t.Type == "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")?.Value;
+            return Json(new { Id = id });
+        }
+    }
+}
